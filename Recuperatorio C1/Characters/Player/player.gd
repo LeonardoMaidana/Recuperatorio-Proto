23ee9f4bbc13274c0bc38ca7222bc1aa9261da26
Leonardo Maidana <leo-lomas1@hotmail.com>
2026-08-10 20:58:31 -0300

    Movimiento base y prueba

--- Recuperatorio C1/Characters/Player/player.gd
+++ Recuperatorio C1/Characters/Player/player.gd
@@ -1,1 +1,31 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var speed: int = 130
+@export var jump_force: int = -280
+
+var gravity: int = 400
+
+func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
+	handle_movement()
+	handle_jump()
+	move_and_slide()
+
+
+func apply_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+
+func handle_movement() -> void:
+	var direction: float = Input.get_axis(
+		"move_left",
+		"move_right"
+	)
+
+	velocity.x = direction * speed
+
+
+func handle_jump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
