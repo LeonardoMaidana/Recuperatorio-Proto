29e5350a47c4858cad1bb41f229e69f74f5b175a
Leonardo Maidana <leo-lomas1@hotmail.com>
2026-08-13 17:51:27 -0300

    Mejora minima del Menu

--- Recuperatorio C1/Scripts/menu.gd
+++ Recuperatorio C1/Scripts/menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var play_button: Button = $MenuCC/MenuVBoxC/PlayButton
+
+
+func _ready() -> void:
+	play_button.grab_focus()
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_01.tscn")
